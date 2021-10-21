cdc1b49fd57a73bce36fba99cd5831ff7cd2e6a1
dovakeen118 <kegillis118@gmail.com>
2021-10-21 11:42:43 -0400

    update react fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,34 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react"
 
 const App = (props) => {
   const [gifs, setGifs] = useState([])
 
+  const fetchGifs = async () => {
+    try {
+      const response = await fetch("/api/v1/gifs")
+      if (!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`)
+      }
+      const responseBody = await response.json()
+      // debugger
+      setGifs(responseBody)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   useEffect(() => {
-    fetch("/api/v1/gifs")
-      .then(response => response.json())
-      .then(bodyOfGifs => {
-        setGifs(bodyOfGifs)
-      })
+    fetchGifs()
   }, [])
 
   const gifImages = gifs.map((gifLink) => {
-    return(
-      <img src={gifLink}  height="200" width="200"></img>
-    )
+    return <img src={gifLink} height="200" width="200"></img>
   })
 
   return (
     <div>
       <h1>Fabulous Images</h1>
       {gifImages}
-
     </div>
   )
 }
